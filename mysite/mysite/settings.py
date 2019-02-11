cea1debeaba7042e7b54d348271ca31a0768c486
Maria Climent-Pommeret <maria@climent-pommeret.red>
2019-02-11 18:19:54 +0100

    Settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,5 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_APP = os.path.basename(PROJECT_APP_PATH)
+PROJECT_ROOT = BASE_DIR = os.path.dirname(PROJECT_APP_PATH)
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
