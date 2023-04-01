ec68715154af0a0d239daceb2f37f26abb1e302e
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-01 13:27:01 +0100

    favicon

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 set -euET -o pipefail
 
+convert favicon.png -resize 16x16 favicon.ico || true
+
 h="$(ipfs cid base32 "$(ipfs add --recursive --hidden --progress --pin=false --ignore-rules-path=.ipfsignore --quieter .)")"
 echo
 echo "ipfs://$h"
