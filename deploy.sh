7b59a0389289dea7e9ebcc567667d2bc953efaf1
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-02 03:55:23 +0100

    Works. Needs more fields, some missing LaTeX packages, input sanitization, a button to download the PDF or API to start a download (to avoid pop-up blocker), and a bit more obvious logging/ability to edit the LaTeX template and/or generated source

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,13 @@ set -euET -o pipefail
 convert favicon.png -resize 16x16 favicon.ico || true
 
 h="$(ipfs cid base32 "$(ipfs add --recursive --hidden --progress --pin=false --ignore-rules-path=.ipfsignore --quieter .)")"
-echo
-echo "ipfs://$h"
+printf \\n
+printf \\n
+printf %s\\n 'Test with:'
+printf \\n
+printf "    ipfs://%s"\\n "$h"
+printf \\n
+printf %s\\n 'Pin with:'
+printf \\n
+printf "    ipfs pin add %s"\\n "$h"
+printf \\n
