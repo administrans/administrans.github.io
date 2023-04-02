0a7eea49468e43cb1e0b67fb35c877d36847a9ac
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-02 19:24:43 +0100

    updated texlive.js, compilation seems to work for empty letter now

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,34 @@ set -euET -o pipefail
 
 convert favicon.png -resize 16x16 favicon.ico || true
 
+if true; then
+(
+  cd extra-packages
+  for i in *.tds.zip; do
+    (
+      d="$(mktemp -d)"
+      ii="$PWD/$i"
+      (cd "$d"; unzip "$ii")
+      cp -a "$d/tex" ../texlive.js/texlive/texmf-dist/
+    )
+  done
+)
+fi
+
+sed -i -e 's/\\ProvidesClass{lettre}\[/\\ProvidesClass{lettre}%[/' texlive.js/texlive/texmf-dist/tex/latex/lettre/lettre.cls
+
+(
+  cd texlive.js
+  find texlive -type d -exec echo {}/. \; | sed 's/^texlive//g' >texlive.lst
+  find texlive -type f | sed 's/^texlive//g' >>texlive.lst
+  for i in texmf-config texmf-dist texmf-var; do
+    (
+      cd texlive/"$i"/
+      ls -R -1 > ls-R
+    )
+  done
+)
+
 h="$(ipfs cid base32 "$(ipfs add --recursive --hidden --progress --pin=false --ignore-rules-path=.ipfsignore --quieter .)")"
 printf \\n
 printf \\n
