546dbcf184b89f383d5f81777a25037085ea507c
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-08 03:41:34 +0100

    relative links, fixed small typos/issues

--- site.js
+++ site.js
@@ -19,7 +19,7 @@ var site_trans_cec = {
       '        <span class="icon-bar"></span>',
       '        <span class="icon-bar"></span>',
       '      </button>',
-      '      <a class="navbar-brand" href="'+site_trans_cec.urlbase+'">Trans Administratif</a>',
+      '      <a class="navbar-brand" href="'+site_trans_cec.urlbase+'#">Trans Administratif</a>',
       '    </div>',
       '    <div class="collapse navbar-collapse" id="myNavbar">',
       '      <ul class="nav navbar-nav">',
