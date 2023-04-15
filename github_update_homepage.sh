2103fef381cf410647277f0d40412fa86b9422ff
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-15 23:19:06 +0100

    Try to auto-update Homepage url

---
+++ github_update_homepage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euET -o pipefail
+
+h="$(ipfs cid base32 "$(ipfs add --recursive --hidden --progress --pin=false --ignore-rules-path=.ipfsignore --quieter .)")"
+curl -L \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $API_TOKEN_FOR_UPDATE_HOMEPAGE"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/trans-cec/trans-cec.github.io \
+  -d '{"name":"trans-cec.gitlab.io", "homepage":"https://'"$h"'.ipfs.dweb.link/"}'
