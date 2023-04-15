9481e5814209aa7d743702ab105223206fd1573f
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-15 23:36:28 +0100

    Install IPFS dependency for github (fix)

--- install-ipfs.sh
+++ install-ipfs.sh
@@ -5,3 +5,4 @@ set -euET -o pipefail
 cd /tmp
 wget https://dist.ipfs.tech/kubo/v0.19.1/kubo_v0.19.1_linux-amd64.tar.gz
 tar -zxf kubo_v0.19.1_linux-amd64.tar.gz
+PATH="/tmp/kubo:$PATH" ipfs init --profile=lowpower
