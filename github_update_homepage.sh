b212bf18b8c88d810346e60b149b09ad956e735e
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-15 23:55:05 +0100

    debug hash, again

--- github_update_homepage.sh
+++ github_update_homepage.sh
@@ -2,7 +2,7 @@
 
 set -euET -o pipefail
 
-h="$(ipfs cid base32 "$(ipfs add --recursive --hidden --pin=false --ignore-rules-path=.ipfsignore --quieter .)")"
+h="$(ipfs cid base32 "$(ipfs add --recursive --pin=false --ignore-rules-path=.ipfsignore --quieter .)")"
 curl -L \
   -X PATCH \
   -H "Accept: application/vnd.github+json" \
