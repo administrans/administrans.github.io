f51408d582b1ae3bde1903e97c046fe2ecd3a79b
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-04-19 22:19:57 +0100

    Forgot to rename repo in script

--- .github/github_update_homepage.sh
+++ .github/github_update_homepage.sh
@@ -41,4 +41,4 @@ curl -L \
   -H "Authorization: Bearer $API_TOKEN_FOR_UPDATE_HOMEPAGE"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/trans-cec/trans-cec.github.io \
-  -d '{"name":"trans-cec.github.io", "homepage":"https://dweb.link/ipfs/'"$h"'"}' > /dev/null
+  -d '{"name":"administrans.github.io", "homepage":"https://dweb.link/ipfs/'"$h"'"}' > /dev/null
